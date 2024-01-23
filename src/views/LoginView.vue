2375696d0b51ac91d5b95fcd4fad02031521ec3a
ArtcalO <niyca14@gmail.com>
2024-01-23 18:42:02 +0200

    init project

---
+++ src/views/LoginView.vue
@@ -0,0 +1,34 @@
+<template>
+	<div>
+			<input
+				type="number"
+				placeholder="x"
+				v-model="a" 
+			>
+			<input
+				type="number"
+				placeholder="x"
+				v-model="b" 
+			>
+			<p>Affichage de la variable dans data {{ a+b }}</p>
+			<button type="submit" @click="calculer">Calculer</button>
+	</div>
+</template>
+
+<script >
+	export default{
+		data(){
+			return{
+				a:0,b:0
+			}
+		},
+		methods:{
+			jhjhjhjh(){
+				alert("cliked")
+			}
+		}
+	}
+</script>
+
+<style scoped>
+</style>
