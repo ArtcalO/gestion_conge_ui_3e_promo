4f35285693fc2ba5fe668f102077cc20a05f0432
ArtcalO <niyca14@gmail.com>
2024-01-30 18:33:03 +0200

    remove all files and components

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,9 @@
 <script >
-import LoginView from "./components/LoginView.vue"
 
-export default{
-    data(){
-        return{
-            isConnected:false
-        }
-    },
-    components:{LoginView}
-}
 </script>
 
 <template>
-    <router-view v-if="isConnected==true"></router-view>
-    <LoginView v-else></LoginView>
+
 </template>
 
 <style src="./styles.css">
--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,10 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import ListeEmploye from "../views/ListeEmploye.vue"
+
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    {
-      path:"/",
-      name:"listeEmploye",
-      component:ListeEmploye
-    }
+
   ]
 })
 
